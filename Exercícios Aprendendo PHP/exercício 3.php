264b37e7b2b79813fe1fe4966dcc8de21258b11e
Luiz  Escobar <54407649+LuizEscobarC@users.noreply.github.com>
2021-01-26 13:57:48 -0400

    Create exercício 3.php

---
+++ Exercícios Aprendendo PHP/exercício 3.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    //incrementos sem usar laço de repetição
+        $potencia = 2;
+        $incremento = 0;
+        ++$incremento;
+        print "$incremento ";
+        ++$incremento;
+        print "$incremento ";
+        ++$incremento;
+        print "$incremento ";
+        ++$incremento;
+        print "$incremento ";
+        ++$incremento;
+        print "$incremento ";
+        print "\n";
+        print 'As potencias são:';
+
+        $potencia *= 2;
+        print "$potencia ";
+        $potencia *= 2;
+        print "$potencia ";
+        $potencia *= 2;
+        print "$potencia ";
+        $potencia *= 2;
+        print "$potencia ";
+    ?>
+</body>
+</html>
