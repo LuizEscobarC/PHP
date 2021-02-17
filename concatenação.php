7a05dc681f8f9a0908d496e3697a2955cadf5664
Luiz  Escobar <54407649+LuizEscobarC@users.noreply.github.com>
2021-02-17 01:40:15 -0400

    .

--- concatenação.php
+++ concatenação.php
@@ -1,13 +1,15 @@
 <!DOCTYPE html>
-<html lang="pt-br">
+<html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
 <body>
-    <?php 
-
+    <?php
+     //concatenando strings
+        print 'bread' . 'fuit';
+        print 'É UM LINDO DIA ' . 'NA VIZINHANÇA';
     ?>
 </body>
 </html>
--- numeros.php
+++ numeros.php
@@ -1,13 +1,14 @@
 <!DOCTYPE html>
-<html lang="pt-br">
+<html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
 <body>
-    <?php 
-
+    <?php
+        /*resto da divisão */
+        print 17 % 3;
     ?>
 </body>
 </html>
